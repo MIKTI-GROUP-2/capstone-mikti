00cbf423993075b3c51a58076ff5176e58c3ac36
davidhnadeak <davidhnadeak@gmail.com>
2024-06-03 19:23:21 +0700

    Wishlist: add JwtAuth to all wishlist routes

--- routes/route.go
+++ routes/route.go
@@ -34,10 +34,10 @@ func NewRoute(c *configs.ProgrammingConfig, uh users.UserHandlerInterface, wh wi
 
 	// Route Wishlist
 	groupWishlist := group.Group("/wishlist")
-	groupWishlist.POST("/create", wh.Create())
-	groupWishlist.GET("", wh.GetAll())
-	groupWishlist.GET("/:id", wh.GetByID())
-	groupWishlist.DELETE("/:event_id/delete", wh.Delete())
+	groupWishlist.POST("/create", wh.Create(), JwtAuth)
+	groupWishlist.GET("", wh.GetAll(), JwtAuth)
+	groupWishlist.GET("/:id", wh.GetByID(), JwtAuth)
+	groupWishlist.DELETE("/:event_id/delete", wh.Delete(), JwtAuth)
 
 	return e
 }
