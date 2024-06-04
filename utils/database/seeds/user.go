abb940efec7bad61ad12bdfc0cd71bba110ff747
Irvan Hauwerich <irvanhau@gmail.com>
2024-06-04 14:10:41 +0700

    Fix Multiple Insert Admin

--- utils/database/seeds/user.go
+++ utils/database/seeds/user.go
@@ -8,14 +8,21 @@ import (
 )
 
 func CreateUser(db *gorm.DB, username, email, phone_number string) error {
-	enkrip := enkrip.New()
-	hashPass, _ := enkrip.HashPassword("password")
-	return db.Create(&users.User{
-		Username:    username,
-		Email:       email,
-		PhoneNumber: phone_number,
-		Password:    hashPass,
-		IsAdmin:     true,
-		Status:      true,
-	}).Error
+	var countData int64
+	db.Table("users").Where("username = ?", "admin").Count(&countData)
+
+	if countData < 0 {
+		enkrip := enkrip.New()
+		hashPass, _ := enkrip.HashPassword("password")
+		return db.Create(&users.User{
+			Username:    username,
+			Email:       email,
+			PhoneNumber: phone_number,
+			Password:    hashPass,
+			IsAdmin:     true,
+			Status:      true,
+		}).Error
+	}
+
+	return nil
 }
